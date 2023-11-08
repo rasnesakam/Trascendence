1f5690f0282a78a80e5472afc410858ecc051bf3
Ensar Makas <ensar.makas@gmail.com>
2023-11-08 10:59:55 +0300

    added api module and removed trascendence_api module

--- backend/trascendence/api/apps.py
+++ backend/trascendence/api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TrascendenceApiConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'trascendence_api'
+    name = 'api'
--- backend/trascendence/urls.py
+++ backend/trascendence/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+#router.register(r'users', include(views.UserViewSet))
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
