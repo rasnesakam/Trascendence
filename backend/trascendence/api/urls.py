4535bdfa4f6333fd2ee9c79fc9a21c3a9e2b43c6
Ensar Makas <ensar.makas@gmail.com>
2023-11-11 20:34:17 +0300

    Added new urls

--- backend/trascendence/api/urls.py
+++ backend/trascendence/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import  path, include
 from trascendence.api.views.UserView import UserView
+from trascendence.api.views import AuthView
+from trascendence.api.views import Hello
 
 urlpatterns = [
     path('users', UserView.as_view()),
-
+    path('auth',AuthView.auth),
+    path("hello",Hello.hi)
 ]
