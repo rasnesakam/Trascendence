5d28c6192c53dbf943b65d2f96e73800b46960c4
Ensar Makas <ensar.makas@gmail.com>
2023-12-03 10:17:49 +0300

    Added serializer for login inputs

--- backend/trascendence/api/serializers.py
+++ backend/trascendence/api/serializers.py
@@ -5,4 +5,8 @@ from .models.User import UserModel
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserModel
-        fields = ["id", "created_at", "updated_at", "username", "email"]
+        fields = ["id", "created_at", "updated_at", "username", "email", "name", "surname", "uid"]
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    access_token = serializers.CharField
