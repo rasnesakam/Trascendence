fbfd4f3d480a33991b7141cca46a07a953b8afad
Ensar Makas <ensar.makas@gmail.com>
2023-12-13 22:03:57 +0300

    Added serializer

--- backend/trascendence/api/serializers.py
+++ backend/trascendence/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models.User import UserModel
+from .models.SerializableModel import SerializableModel
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,3 +11,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 class LoginSerializer(serializers.ModelSerializer):
     access_token = serializers.CharField
+
+
+def serialize_json(model: SerializableModel):
+    return model.to_json()
