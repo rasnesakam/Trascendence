59c001df38662cf125061bae4807e559615d0699
Ensar Makas <ensar.makas@gmail.com>
2023-12-13 18:58:30 +0300

    Lots of changes too

--- backend/trascendence/api/urls.py
+++ backend/trascendence/api/urls.py
@@ -1,11 +1,16 @@
 from django.urls import  path, include
+from django.views.generic.base import RedirectView
 from trascendence.api.views.UserView import UserView
 from trascendence.api.views import AuthView
 from trascendence.api.views import Hello
 
+# Write decorator to restrict http methods for requests
+# auth_redirect -> 42/oauth -> auth/token/code -> page
+
 urlpatterns = [
     path('users', UserView.as_view()),
-    path('auth',AuthView.auth),
-    path('auth/sign-in',AuthView.sign_in),
-    path("hello",Hello.hi)
+    path('auth/sign-in', AuthView.sign_in),
+    path('auth/sign-up', AuthView.sign_up),
+    path('auth_redirect', AuthView.OAuth),
+    path('auth/token/code', AuthView.token)
 ]
