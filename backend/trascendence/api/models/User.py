9a3c1b75aaf11545a544fa71e500036d4bffc429
Ensar Makas <ensar.makas@gmail.com>
2023-12-13 22:02:53 +0300

    Removed updated_at field from db

--- backend/trascendence/api/models/User.py
+++ backend/trascendence/api/models/User.py
@@ -1,14 +1,15 @@
 import django.utils.timezone
 from django.db import models
-from datetime import datetime
+from .SerializableModel import SerializableModel
 import uuid
 
 
-class UserModel(models.Model):
-    id = models.CharField(max_length=36, default=uuid.uuid4(), primary_key=True)
+class UserModel(models.Model, SerializableModel):
+    id = models.CharField(max_length=36, default=uuid.uuid4, primary_key=True)
     created_at = models.DateTimeField(default=django.utils.timezone.now)
-    updated_at = models.DateTimeField(default=django.utils.timezone.now)
     intraId = models.IntegerField()
     username = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
     avatarURI = models.CharField(max_length=200)
+
+
