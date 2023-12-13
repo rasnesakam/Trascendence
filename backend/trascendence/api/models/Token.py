145abd5fc7a3a30395bc9cebc255bd3c039d9f86
Ensar Makas <ensar.makas@gmail.com>
2023-12-13 18:58:02 +0300

    Updated models

--- backend/trascendence/api/models/Token.py
+++
@@ -1,7 +0,0 @@
-from trascendence.api.models import BaseModel
-from django.db import models
-from .User import UserModel
-
-class Token(BaseModel.BaseModel):
-    token = models.CharField()
-    user_id = models.ForeignKey(UserModel, on_delete=models.CASCADE)
