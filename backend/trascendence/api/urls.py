b2f8d52141bbd10dcca32b0169f916aaf8f44213
Ensar Makas <ensar.makas@gmail.com>
2023-12-19 23:01:24 +0300

    added urls

--- backend/trascendence/api/urls.py
+++ backend/trascendence/api/urls.py
@@ -9,8 +9,22 @@ from trascendence.api.views import Hello
 
 urlpatterns = [
     path('users', UserView.as_view()),
+
+    # Authentication
     path('auth/sign-in', AuthView.sign_in),
     path('auth/sign-up', AuthView.sign_up),
     path('auth_redirect', AuthView.OAuth),
-    path('auth/token/code', AuthView.token)
+    path('auth/token/code', AuthView.token),
+
+    # Interactions
+    path('interacts/friends/<username>'),
+    path('interacts/friends/<username>/add'),
+    path('interacts/friends/<username>/delete/<username>'),
+    path('interacts/invitation/<username>'),
+    path('interacts/invitation/<username>/<invite_code>/accept'),
+    path('interacts/invitation/<username>/<invite_code>/delete'),
+    path('interacts/blacklist/<username>'),
+    path('interacts/blacklist/<username>/add'),
+    path('interacts/blacklist/<username>/<user>/delete')
+
 ]
