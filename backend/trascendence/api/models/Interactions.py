15f06e293bb26d818b8be291f737128c9a8e9c9d
Ensar Makas <ensar.makas@gmail.com>
2023-12-19 16:58:14 +0300

    Updated models

--- backend/trascendence/api/models/Interactions.py
+++ backend/trascendence/api/models/Interactions.py
@@ -15,15 +15,15 @@ def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
 class Friends(models.Model, SerializableModel):
     id = models.CharField(max_length=36, default=uuid.uuid4, primary_key=True)
     created_at = models.DateTimeField(default=django.utils.timezone.now)
-    user_pair_1 = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    user_pair_2 = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    user_pair_1 = models.ForeignKey(UserModel, related_name="%(class)s_user_pair_1", on_delete=models.CASCADE)
+    user_pair_2 = models.ForeignKey(UserModel, related_name="%(class)s_user_pair_2", on_delete=models.CASCADE)
 
 
 class FriendInvitation(models.Model, SerializableModel):
     id = models.CharField(max_length=36, default=uuid.uuid4, primary_key=True)
     created_at = models.DateTimeField(default=django.utils.timezone.now)
-    origin = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    target = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    origin = models.ForeignKey(UserModel, related_name="%(class)s_origin", on_delete=models.CASCADE)
+    target = models.ForeignKey(UserModel, related_name="%(class)s_target", on_delete=models.CASCADE)
     invite_code = models.CharField(max_length=6, default=id_generator)
     note = models.CharField(max_length=400)
 
@@ -31,6 +31,6 @@ class FriendInvitation(models.Model, SerializableModel):
 class BlackList(models.Model, SerializableModel):
     id = models.CharField(max_length=36, default=uuid.uuid4, primary_key=True)
     created_at = models.DateTimeField(default=django.utils.timezone.now)
-    issuer = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    issuer = models.ForeignKey(UserModel, related_name="%(class)s_issuer", on_delete=models.CASCADE)
+    user = models.ForeignKey(UserModel, related_name="%(class)s_user", on_delete=models.CASCADE)
 
