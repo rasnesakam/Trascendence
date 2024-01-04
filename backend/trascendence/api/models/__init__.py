1cb10158a8079ee0ae2ca8909357ff07db37bfa9
Ensar Makas <ensar.makas@gmail.com>
2024-01-04 15:48:08 +0300

    detailed import statements

--- backend/trascendence/api/models/__init__.py
+++ backend/trascendence/api/models/__init__.py
@@ -1,4 +1,4 @@
-from .Interactions import *
-from .User import *
-from .Tournaments import *
-from .Matches import *
+from .Interactions import Friends, FriendInvitation, BlackList
+from .User import UserModel
+from .Tournaments import TournamentInvitations, TournamentPlayers, Tournaments
+from .Matches import Matches, MATCH_WINNERS
