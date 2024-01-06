4a272666ecc932adb0aa7d54ae12679fc3557c26
Ensar Makas <ensar.makas@gmail.com>
2024-01-06 19:00:36 +0300

    Added tournament players capacity

--- backend/trascendence/api/models/Tournaments.py
+++ backend/trascendence/api/models/Tournaments.py
@@ -3,6 +3,7 @@ from django.db import models
 from .SerializableModel import SerializableModel
 from .User import UserModel
 import uuid
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Tournaments(models.Model, SerializableModel):
@@ -11,6 +12,10 @@ class Tournaments(models.Model, SerializableModel):
     tournament_name = models.CharField(max_length=50)
     created_user = models.ForeignKey(UserModel, related_name="%(class)s_created_user", on_delete=models.CASCADE)
     starts_on = models.DateTimeField
+    players_capacity = models.IntegerField(max_length=8, default=8, validators=[
+        MaxValueValidator(8),
+        MinValueValidator(4)
+    ])
 
 
 class TournamentPlayers(models.Model, SerializableModel):
