596492d06e359b0baf56dd5a2fc4c6d0cb10437c
Ensar Makas <ensar.makas@gmail.com>
2024-01-15 17:01:24 +0300

    Models updates

--- backend/trascendence/api/models/__init__.py
+++ backend/trascendence/api/models/__init__.py
@@ -1,5 +1,5 @@
 from .User import UserModel
-from .Interactions import Friends, FriendInvitation, BlackList
-from .Tournaments import TournamentInvitations, TournamentPlayers, Tournaments
+from .InteractionModels import Friends, FriendInvitation, BlackList
+from .TournamentModels import TournamentInvitations, TournamentPlayers, Tournaments
 from .Matches import Matches, MATCH_WINNERS
 
--- backend/trascendence/api/models/shared_functions.py
+++ backend/trascendence/api/models/shared_functions.py
@@ -2,7 +2,6 @@ import random
 import string
 
 
-
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
 
+    return ''.join(random.choice(chars) for _ in range(size))
