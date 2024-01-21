08b42de43d63031350f842a1d4816af7c609a5f8
Ensar Makas <ensar.makas@gmail.com>
2024-01-21 18:01:10 +0300

    updated model and migrations

--- backend/trascendence/api/models/tournament_models/TournamentInvitations.py
+++ backend/trascendence/api/models/tournament_models/TournamentInvitations.py
@@ -6,13 +6,9 @@ from .Tournaments import Tournaments
 import uuid
 
 
-def generate_invite_code():
-    id_generator(8)
-
-
 class TournamentInvitations(models.Model, SerializableModel):
     id = models.CharField(max_length=36, default=uuid.uuid4, primary_key=True)
     target_user = models.ForeignKey(UserModel, related_name="%(class)s_target_user", on_delete=models.CASCADE)
     tournament = models.ForeignKey(Tournaments, related_name="%(class)s_tournament_id", on_delete=models.CASCADE)
-    invite_code = models.CharField(max_length=8, default=generate_invite_code, unique=True)
+    invite_code = models.CharField(max_length=6, default=id_generator)
     message = models.CharField(max_length=400)
