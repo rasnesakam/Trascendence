b2816b7ffdcde0e5cc5502474dff613a3d7a8c1d
Ensar Makas <ensar.makas@gmail.com>
2024-01-22 13:16:55 +0300

    Added accept endpoint implementation

--- backend/trascendence/api/views/TorunamentsView.py
+++ backend/trascendence/api/views/TorunamentsView.py
@@ -1,6 +1,6 @@
 from django.views.decorators.http import require_http_methods
 from trascendence.middleware.auth import authorize
-from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseNotFound, HttpResponseServerError
+ from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseNotFound, HttpResponseServerError
 from trascendence.api.models.User import UserModel
 from trascendence.api.models.tournament_models import TournamentPlayers, TournamentInvitations, Tournaments
 
@@ -17,14 +17,34 @@ def get_tournament_invitations(request: HttpRequest, username: str) -> JsonRespo
 @require_http_methods(['GET'])
 @authorize
 def get_tournament_invitation(request: HttpRequest, invitationcode: str) -> JsonResponse | HttpResponseNotFound:
-    pass
+    tournament_invitation = TournamentInvitations.objects.get(invite_code__exact=invitationcode)
+    if tournament_invitation is not None:
+        return HttpResponseNotFound(str({"message": f"No invitation found with code: {invitationcode}"}))
+    return JsonResponse({"message": ""}, status=200)
 
 
 @require_http_methods(['POST'])
 @authorize
 def accept_tournamet(request: HttpRequest, invitationcode: str) -> JsonResponse | HttpResponseNotFound:
-    pass
+    tournament_invitation = TournamentInvitations.objects.get(invite_code__exact=invitationcode)
 
+    if tournament_invitation is None:
+        return HttpResponseNotFound(str({"message": f"No invitation found with code: {invitationcode}"}))
+    user = tournament_invitation.target_user
+    tournament = tournament_invitation.tournament
+    unpaired_user = TournamentPlayers.objects.get(tournament=tournament_invitation.tournament, has_pair=False)
+    if unpaired_user is not None:
+        TournamentPlayers.objects.create(
+            tournament=tournament, user=tournament_invitation.target_user, has_pair=True, pair_user=unpaired_user
+        )
+        unpaired_user.has_pair = True
+        unpaired_user.pair_user = user
+        unpaired_user.save()
+    else:
+        TournamentPlayers.objects.create(
+            tournament=tournament, user=tournament_invitation.target_user, has_pair=False, pair_user=None
+        )
+    return JsonResponse({"message": "You are now participated in the tournament."}, status=200)
 
 @require_http_methods(['DELETE'])
 @authorize
