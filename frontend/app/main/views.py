2877656abee98eb6ca1deff911323cb609377954
Ensar Makas <ensar.makas@gmail.com>
2024-02-08 22:35:30 +0300

    Added url redirection support

--- frontend/app/main/views.py
+++ frontend/app/main/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "index.html", {'page_requested': "pages/main.html"})
+routes = {
+    '': 'main.html',
+    'game': 'game.html',
+    'login': 'login.html',
+    'tournament': 'tournament.html'
+}
+
+
+def get_requested_file(url: str, routes: dict) -> str:
+    query_param_index = url.find('?')
+    url_dir = url[: query_param_index if query_param_index > 0 else len(url)]
+    sub_url = url_dir.split("/")[0]
+    for route_dir in routes.keys():
+        if route_dir == sub_url:
+            return routes.get(route_dir)
+    return "error-404.html"
+
+
+def home(request, url):
+    page = get_requested_file(url, routes)
+    return render(request, "index.html", {'page_requested': f"pages/{page}"})
