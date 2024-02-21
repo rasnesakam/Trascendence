c321b327cc9ecdf651e388b45d87513b2a3b4941
Kaano1 <ahmetyumusakdiken13@gmail.com>
2024-02-21 22:58:20 +0300

    login fixed

--- frontend/app/main/templates/static/js/login.js
+++ frontend/app/main/templates/static/js/login.js
@@ -1,28 +1,30 @@
 var myUrl;
 
 (function () {
-  myUrl = window.location.search;
-  searchParams = new URLSearchParams(myUrl)
-
-  console.log("code is: " + searchParams.get("code"));
+  takeUrl();
 })();
 
 function takeUrl() {
-  if (myUrl.includes("id")) {
-	var findIndex = myUrl.index("id");
-	var key = myUrl.substring(findIndex);
-	
-	fetch("https://api.example.com/data", 
+	myUrl = window.location.search;
+	searchParams = new URLSearchParams(myUrl)
+
+	console.log("myUrl: " + myUrl);
+	fetch("http://localhost/api/auth/sign-in/42", 
 	{
 		method: 'POST',
 		headers: {
 			'Content-type': 'application/json'
-		}
+		},
+		body: JSON.stringify({
+			"code": searchParams.get("code")
+		})
 	})
 	.then((response) => {
 		if (!response.ok)
 			return new Error("Respone is not ok");
 		localStorage.setItem('userToken', response);
+		window.location.href = "/";
+		console.log("response: " + response);
 		return (response);
 	})
 	.catch(error => {
@@ -30,7 +32,4 @@ function takeUrl() {
 	});
 
     console.log("String içinde 'id' bulundu.");
-  } else {
-    console.log("String içinde 'id' bulunamadı.");
-  }
 }
