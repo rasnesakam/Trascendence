e4596db321751e4ffc92bd7b26d497970cedb41c
Ensar Makas <ensar.makas@gmail.com>
2024-02-27 11:38:30 +0000

    Added postgresql configuration

--- backend/trascendence/settings.py
+++ backend/trascendence/settings.py
@@ -82,9 +82,13 @@ ASGI_APPLICATION = 'trascendence.asgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "r00T1453!",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
