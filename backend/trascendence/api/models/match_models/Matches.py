c00b1952cfbff096daf010019aac8adc0f7e74b6
Ensar Makas <ensar.makas@gmail.com>
2024-02-27 19:45:38 +0300

    Fields updated

--- backend/trascendence/api/models/match_models/Matches.py
+++ backend/trascendence/api/models/match_models/Matches.py
@@ -15,7 +15,9 @@ MATCH_WINNERS = (
 class Matches(models.Model, SerializableModel):
     id = models.CharField(max_length=36, default=uuid.uuid4, primary_key=True)
     tournament = models.ForeignKey(Tournaments, related_name="%(class)s_tournament_id", on_delete=models.CASCADE, blank=True, null=True)
-    home = models.ForeignKey(UserModel, related_name="%(class)s_home", on_delete=models.CASCADE)
+    home = models.ForeignKey(UserModel, related_name="%(class)s_home", on_delete=models.CASCADE),
+    score_home = models.IntegerField
     away = models.ForeignKey(UserModel, related_name="%(class)s_away", on_delete=models.CASCADE)
+    score_away = models.IntegerField
     winner = models.CharField(max_length=1, choices=MATCH_WINNERS, default="u")
 
