cd024702e7871cb6fc056b45f386127736c6703e
Ensar Makas <ensar.makas@gmail.com>
2024-02-28 22:22:45 +0000

    Implementing creating tournament

--- backend/trascendence/api/views/TorunamentsView.py
+++ backend/trascendence/api/views/TorunamentsView.py
@@ -1,11 +1,11 @@
 from django.views.decorators.http import require_http_methods
-
+import json
 from trascendence.api.models import TournamentMatches
 from trascendence.middleware.auth import authorize
 from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseNotFound, HttpResponseServerError
 from trascendence.api.models.User import UserModel
 from trascendence.api.models.tournament_models import TournamentPlayers, TournamentInvitations, Tournaments
-from trascendence.middleware.validators import request_body, str_field
+from trascendence.middleware.validators import request_body, str_field, list_field
 
 
 @require_http_methods(['GET'])
@@ -110,11 +110,29 @@ def get_tournament_matches(request: HttpRequest, tournamentcode: str) -> JsonRes
     content_type="application/json",
     fields={
         "tournamentName": str_field(max_length=30, required=True),
-        #"users": list_field
+        "users": list_field(required=True),
+        "capacity": number_field(max_length=8, min_length=4, required=True)
     }
 )
-def create_tournament(request: HttpRequest) -> JsonResponse:
-    return JsonResponse({"message": "under construction"}, status=500)
+def create_tournament(request: HttpRequest, content) -> JsonResponse:
+    try:
+        founder_user = UserModel.objects.get(username=request.auth_info["sub"])
+        participated_users = list()
+        usernames = content.get("users", [])
+        if len(usernames) != content["capacity"]:
+            return JsonResponse(json.dumps({"message":f"Unmatched user list. capacity is {content["capacity"]} but {len(usernames)} user invited"}), status=400)
+        for username in usernames:
+            try:
+                participated_user = UserModel.objects.get(username__exact=username)
+                participated_users.append(participated_user)
+            except UserModel.DoesNotExist:
+                return JsonResponse(json.dumps({"message": f"User {username} not found"}), 404)
+        tournament_name = content["tournamentName"]
+        # create tournament
+        tournament = Tournaments.objects.create(tournament_name=tournament_name, created_user=founder_user, players_capacity=content["capacity"])
+        # Invite userlist
+    except UserModel.DoesNotExist:
+        return JsonResponse(json.dumps({"message": "User not found"}), status=404)
 
 
 @require_http_methods(['DELETE'])
