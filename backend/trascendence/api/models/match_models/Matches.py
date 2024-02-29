7c7f26e0c6a2d0ea104da15c3b7796d90b6abac4
Ahmet Kaan Yumuşakdiken <89842738+Kaano1@users.noreply.github.com>
2024-02-29 01:57:58 +0000

    updated field of matches

--- backend/trascendence/api/models/match_models/Matches.py
+++ backend/trascendence/api/models/match_models/Matches.py
@@ -8,7 +8,7 @@ import uuid
 class Matches(models.Model, SerializableModel):
     id = models.CharField(max_length=36, default=uuid.uuid4, primary_key=True)
     tournament = models.ForeignKey(Tournaments, related_name="%(class)s_tournament_id", on_delete=models.CASCADE, blank=True, null=True)
-    home = models.ForeignKey(UserModel, related_name="%(class)s_home", on_delete=models.CASCADE),
+    home = models.ForeignKey(UserModel, related_name="%(class)s_home", on_delete=models.CASCADE)
     score_home = models.IntegerField(default=0)
     away = models.ForeignKey(UserModel, related_name="%(class)s_away", on_delete=models.CASCADE)
     score_away = models.IntegerField(default=0)
