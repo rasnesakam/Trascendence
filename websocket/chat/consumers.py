87292aca6c0240ff79a7a2c9b04b80d66849f1e6
hnur-eris <erisshatice6@gmail.com>
2024-03-02 18:59:31 +0300

    merged cnm

--- websocket/chat/consumers.py
+++ websocket/chat/consumers.py
@@ -4,7 +4,16 @@ from asgiref.sync import async_to_sync
 from django.contrib.auth.models import AnonymousUser
 from .models import Message
 from django.utils.dateformat import DateFormat
-import request
+from django.contrib.auth.tokens import default_token_generator
+from django.conf import settings
+from django.utils.http import urlsafe_base64_decode
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
+import requests
+import jwt
+
+# TODO: bu APP_NAME değerini .env dosyasından çek
+APP_NAME = "localhost"
 
 class ChatConsumer(WebsocketConsumer):
     def fetch_messages(self, data):
@@ -36,6 +45,27 @@ class ChatConsumer(WebsocketConsumer):
                 return
             #kullanıcı oluşuturulacak database
             # token doğrulanacak -backend
+            response = requests.get(f'http://{APP_NAME}/api/auth/token', headers={"Authorization": f"Bearer {token}"})
+            print(response)
+            print(response.json())
+            if response.status_code == 200:
+                # Burada token geçerli
+                #kullanıcı oluşcak
+                try:
+                    user_id = jwt.decode(token, algorithms=['HS256'])
+                    user = User.objects.get(id = user_id)
+                except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+                    self.send(text_data=json.dumps({
+                        'error': 'Invalid token or user not found.'
+                    }))
+                    return
+            else:
+                # burada token geçersiz
+                self.send(text_data=json.dumps({
+                    'error': 'Invalid token.'
+                }))
+                return
+
         message = Message.objects.create(
             author=user, 
             content=data['message'])
@@ -85,7 +115,11 @@ class ChatConsumer(WebsocketConsumer):
     }
     def receive(self, text_data):
         data = json.loads(text_data)
-        self.types[data['type']](self, data)
+        message_type = data.get("type", None)
+        if message_type is not None:
+            self.types[data["type"]](self, data)
+        else:
+            self.send(json.dumps({"message": "You need to set type for message type"}))
         
     def send_chat_message(self, message): 
         async_to_sync(self.channel_layer.group_send)(
