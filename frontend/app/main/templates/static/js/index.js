b0ee3236d883cad2b3792afaf8ca87a072aca4da
Ahmet Kaan Yumuşakdiken <89842738+Kaano1@users.noreply.github.com>
2024-03-04 13:11:17 +0000

    loadUserInformation function is very useful

--- frontend/app/main/templates/static/js/index.js
+++ frontend/app/main/templates/static/js/index.js
@@ -1,7 +1,7 @@
 //İndex.js
 //sayısal değerlere define değerler atanabilir
 if (window.location.pathname == "/")
-  loadUserInformation(0);
+  main_load()
 
 const webRoute = {
   404: "/static/pages/error.html",
@@ -153,30 +153,37 @@ function showTournament(tournaments) {
 
 //showMatch();
 
-async function loadUserInformation(id) {
-  var userAccess = JSON.parse(localStorage.getItem(0));
-  var access_token = userAccess.access_token;
-  var userIdentity = await fetch(`http://localhost/api/profile/${userAccess.user.username}`, {
+function main_load()
+{
+  let userAccess = JSON.parse(localStorage.getItem(0));
+  let username = userAccess.user.username;
+  let access_token = userAccess.user.access_token;
+
+  loadUserInformation(username, access_token);
+}
+
+async function loadUserInformation(username, access_token) {
+  let userIdentity = await fetch(`http://localhost/api/profile/${username}`, {
     headers: {
       "Authorization": "Bearer " + access_token,
     },
   }).then(data => data.json());
 
-  var dataTournament = await fetch(
-    `http://localhost/api/tournaments/${userAccess.user.username}`, {
+  let dataTournament = await fetch(
+    `http://localhost/api/tournaments/${username}`, {
     headers: {
       "Authorization": "Bearer " + access_token,
     },
   }).then(data => data.json());
 
-  var dataMatches = await fetch(
-    `http://localhost/api/matches/${userAccess.user.username}`, {
+  let dataMatches = await fetch(
+    `http://localhost/api/matches/${username}`, {
     headers: {
       "Authorization": "Bearer " + access_token,
     },
   }).then(data => data.json());
 
-  localStorage.setItem(id + 1, JSON.stringify(userIdentity));
+  localStorage.setItem(1, JSON.stringify(userIdentity));
   document.getElementById("nickname").innerHTML = userIdentity.username;
   document.getElementById("pr-name").innerHTML = userIdentity.name; //username html
   document.getElementById("pr-surname").innerHTML = userIdentity.surname; //surname add html
@@ -185,17 +192,8 @@ async function loadUserInformation(id) {
   document.getElementById("total_match").innerHTML = dataMatches.length; //match added html
   document.getElementById("enemy").innerHTML = userIdentity.rival;
 
-  if (id == 2) {
-    let tournamentWin = winCount(dataTournament, userIdentity.username);
-    let tournamentLose = dataTournament.content.size() - tournamentWin;
-    let matchWin = 2;
-    let matchLose = 3 - matchWin;
-
-    setRate(tournamentWin, tournamentLose, "myPieChart");
-    setRate(matchWin, matchLose, "myPieChart2");
-    showTournament(dataTournament);
-    //showMatch();
-  }
+  let resultData = {userIdentity, dataTournament, dataMatches};
+  return (resultData);
 }
 
 function saveUserInformation() {
