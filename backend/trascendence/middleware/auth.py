5566714480c17e531888ea534edd84817cb3c803
Ensar Makas <ensar.makas@gmail.com>
2024-03-07 11:28:24 +0000

    auth decorator now injects which user authenticated

--- backend/trascendence/middleware/auth.py
+++ backend/trascendence/middleware/auth.py
@@ -1,6 +1,6 @@
 from django.http import HttpRequest, JsonResponse
 from django.http import HttpResponse
-
+from trascendence.api.models.User import UserModel
 from trascendence.core import validate_token
 
 
@@ -19,10 +19,17 @@ def authorize(request_view):
         if token is None:
             return JsonResponse({"message": "this content is not allowed"}, status=401)
         try:
-            print(f">{token}<")
-            auth_info = validate_token(token)
-            setattr(request, "auth_info", auth_info)
-            return request_view(request, *args, **kwargs)
+            token_info = validate_token(token)
+            try:
+                user = UserModel.objects.get(id=token_info['sub'])
+                auth_info = object()
+                setattr(auth_info, "token_info", token_info)
+                setattr(auth_info, "user", user)
+                setattr(auth_info, "token", token)
+                setattr(request, "auth_info", auth_info)
+                return request_view(request, *args, **kwargs)
+            except UserModel.DoesNotExist:
+                return JsonResponse({"message": "No such user associated with this token."}, status=401)
         except Exception as e:
             return JsonResponse({"message": f"Token couldn't verified. Reason is: {str(e)}"}, status=401)
 
