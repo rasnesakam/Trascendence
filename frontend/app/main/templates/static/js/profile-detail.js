6bbe90bcd6753127725d213e281eb891eebd84d5
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2024-03-13 01:15:21 +0000

    ok

--- frontend/app/main/templates/static/js/profile-detail.js
+++ frontend/app/main/templates/static/js/profile-detail.js
@@ -9,16 +9,3 @@ function updateChart(win, lose) {
 }
 
 updateChart(winPercentage, losePercentage);
-
-
-function profile_load() {
-    let pathname = window.location.pathname;
-    let part = pathname.split("/");
-    let data = JSON.parse(localStorage.getItem(0));
-    let access_token = data.access_token;
-
-    console.log("data page", data);
-    loadUserInformation(part[2], access_token);
-    isMyFriend(data.user.username, part[2], access_token);
-    isBlock(data.user.username, part[2], access_token);
-}
